e6355b9d3fa5e5134113e47650724e44b57b24c5
syedfazil900 <fazilking900@gmail.com>
2021-12-15 20:49:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("#searchForm");
+let div = document.querySelector("div");
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = {
+        params: {
+            q: searchTerm
+        }
+    }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`,config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+})
+
+const makeImages = function(shows) {
+    removeImages();
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src= result.show.image.medium;
+            div.append(img);
+        }
+  
+    }
+}
+const removeImages = () =>
+{
+    div.querySelectorAll('*').forEach(n => n.remove());
+
+}
